e965bf941424eb1284fe471ce7443b1b44f1dd12
Timothy L Jordan II <timjae@gmail.com>
2023-10-20 13:45:52 -0400

    added installer.

--- search@timjae/search_provider.js
+++ search@timjae/search_provider.js
@@ -10,10 +10,10 @@ function perform_search(pattern) {
 function push_results(results) {
 	if (!results.match(/^(((?!\-))(xn\-\-)?[a-z0-9\-_]{0,61}[a-z0-9]{1,1}\.)*(xn\-\-)?([a-z0-9\-]{1,61}|[a-z0-9\-]{1,30})\.[a-z]{2,}$/g)) {
 		// Google Search
-		var file = Gio.file_new_for_path('/home/timothy/.local/share/cinnamon/search_providers/search@timjae/google.png');
+		var file = Gio.file_new_for_path('.local/share/cinnamon/search_providers/search@timjae/google.png');
 		var gicon = new Gio.FileIcon({ file: file });
 		var myicon = new St.Icon({gicon: gicon, icon_size: 32, icon_type: St.IconType.FULLCOLOR, reactive: true, track_hover: true, style_class: 'applet-icon'});
-	
+
 		var pattern = results.replace(" ", "+");
 		var googurl = "www.google.com/search?q=" + pattern;
 		var pattern_array = new Array();
@@ -25,7 +25,7 @@ function push_results(results) {
 		send_results(pattern_array);
 	} else {
 		// Domain Search
-		var file = Gio.file_new_for_path('/home/timothy/.local/share/cinnamon/search_providers/search@timjae/website.png');
+		var file = Gio.file_new_for_path('.local/share/cinnamon/search_providers/search@timjae/website.png');
 		var gicon = new Gio.FileIcon({ file: file });
 		var myicon = new St.Icon({gicon: gicon, icon_size: 32, icon_type: St.IconType.FULLCOLOR, reactive: true, track_hover: true, style_class: 'applet-icon'});
 
